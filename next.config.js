1694353c55e15127fd6c813c70a010a3b32d96df
batyr2231 <aawwstoyak21@gmail.com>
2025-10-07 23:38:49 +0500

    Fix next.config.js

--- next.config.js
+++ next.config.js
@@ -1,17 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    serverActions: true,
-  },
   images: {
     domains: ['firebasestorage.googleapis.com'],
-  },
-  // Исключаем server из сборки
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
   },
 }
 
